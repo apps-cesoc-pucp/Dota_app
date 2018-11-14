0291cbd47ed70c2d322f63ef380510da59b59667
DanielGuzmanT <jhairguz.t@gmail.com>
2018-11-14 18:43:52 -0500

    error al imprimir datos de heroes - FALTA agregar imagenes

--- DotaApp/app/src/main/java/com/cesoc/apps/android/dotaapp/MainActivity.java
+++ DotaApp/app/src/main/java/com/cesoc/apps/android/dotaapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.cesoc.apps.android.dotaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.cesoc.apps.android.dotaapp.DotaApi.ApiConsumer;
 import com.cesoc.apps.android.dotaapp.DotaApi.Heroe;
@@ -11,11 +12,19 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Heroe> heroesList;
+    TextView tv_hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_hello = findViewById(R.id.hello);
         heroesList = ApiConsumer.chargeHeroesList();
+        tv_hello.setText(" ");
+        for(Heroe heroe : heroesList){
+            tv_hello.append(heroe.getName());
+            tv_hello.append(heroe.getAttack_type());
+        }
+
     }
 }
